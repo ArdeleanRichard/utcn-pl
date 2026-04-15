946b3f1bb86092c9bfb9600ebcfe604569bf7cf9
Richard Ardelean <ardeleaneugenrichard@gmail.com>
2026-04-15 08:14:01 +0300

    c7,c8,c9

--- course/c09_code.pl
+++ course/c09_code.pl
@@ -44,21 +44,15 @@ q2.
 
 
 hanoi(N):-
-	init_list(N,List), 				% List = [5, 4, 3, 2, 1]
-	init_tower(in,List),			% ?- init_list(3, List), init_pole(in, List), listing(in/1). 	% Creates tower as: in(1), in(2)
+	init_tower(in,N),				% ?- init_tower(in, List), listing(in/1). 	% Creates tower as: in(1), in(2)
 	move_discs(N,in,int,out).		% in,int,out = x,y,z towers
 
-
-init_list(1,[1]):-!.
-init_list(N,[N|Rest]):- 
-	N1 is N-1,
-	init_list(N1,Rest).
-
-init_tower(_,[]):-!.
-init_tower(T,[N|L]):-
+init_tower(_,0):-!.
+init_tower(T,N):-
 	D=..[T,N],			% T name of tower, as x in example; N size   	% D=..[in, N] <--> D=in(1) / D=in(2) ...
 	asserta(D), 		% put on top									% asserta(in(1)), assert(in(2)), ...
-	init_tower(T,L).
+	N1 is N-1,
+	init_tower(T,N1).
 	
 
 move_discs(0,_,_,_):-!.

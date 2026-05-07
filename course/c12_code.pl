0fbbe96fe4d85811a0dbef8f8e1cba2cac7733a0
Richard Ardelean <ardeleaneugenrichard@gmail.com>
2026-05-07 08:55:35 +0300

    c12

--- course/c12_code.pl
+++ course/c12_code.pl
@@ -175,3 +175,84 @@ member_il(X, [_|T]):- member_il(X, T).
 
 
 
+%--------------------------------------------------
+% Additionals %
+%--------------------------------------------------
+% How to collect pairs in v1
+iso_graph5(L1,L2,R):- eq_perm5(L1,L2,eq_neighb5), collect(R).
+
+collect([p(N1,N2)|R]):-
+	retract(p(N1,N2)),!,
+	collect(R).
+collect([]).
+
+eq_perm5([H1|T1],L2,EQ):- 	% is the perm predicate
+	delete(H2,L2,T2),		% with an equivalence predicate
+	P=..[EQ,H1,H2],			% which is created here
+	call(P),				% and called here
+	eq_perm5(T1,T2,EQ).
+eq_perm5([],[],_).
+
+
+eq_neighb5(n(N1,L1),n(N2,L2)):- 	% 2 neighb pairs are equivalent
+	eq_node5(N1,N2),				% if the nodes are equivalent
+	eq_perm5(L1,L2,eq_node5). 		% and the neighb lists are equivalent
+
+
+eq_node5(N1,N2):-			% if nodes N1 and N2 already form a pair in the akb
+	p(N1,N2).					% the evaluation continues
+eq_node5(N1,_):-			% if N1 forms a pair with some OTHER node
+	p(N1,_),!,					% we get an inconsistency, so should NOT allow
+ 	fail.						% (N1,N2) form a pair, so, fail to backtrack
+eq_node5(_,N2):-			% symmetric on N2
+	p(_,N2),!,
+	fail.
+eq_node5(N1,N2):-			% if you reach here, the is no inconsistency
+	asserta(p(N1,N2)). 			% hence pair N1, N2 is a legitimate one.
+eq_node5(N1,N2):-			% if at a later moment, although pair N1, N2 is a legitimate one,
+    retract(p(N1,N2)),!, 		% the reasoning cannot conclude,
+    fail.						% remove it from the akb, and
+								% fail to backtrack and
+								% resume execution WITHOUT the pair in the akb
+
+
+
+
+
+
+
+% How to implement efficiently ***
+iso_graph(L1,L2,R):- eq_perm(L1,L2,eq_neighb), !, collect(R).
+
+eq_perm(L1,L2,EQ):- 						% is the perm predicate
+	length(L1, Len), length(L2, Len),		% *** check lengths
+	L1=[H1|T1],						
+	delete(H2,L2,T2),						% with an equivalence predicate
+	P=..[EQ,H1,H2],							% which is created here
+	call(P),								% and called here
+	eq_perm(T1,T2,EQ).
+eq_perm([],[],_):-!.
+
+
+eq_neighb(n(N1,L1),n(N2,L2)):- 		% 2 neighb pairs are equivalent
+	eq_node(N1,N2),					% if the nodes are equivalent
+	eq_perm(L1,L2,eq_node). 		% and the neighb lists are equivalent
+
+
+eq_node(N1,N2):-			% if nodes N1 and N2 already form a pair in the akb
+	p(N1,N2).					% the evaluation continues
+eq_node(N1,_):-				% if N1 forms a pair with some OTHER node
+	p(N1,_),!,					% we get an inconsistency, so should NOT allow
+ 	fail.						% (N1,N2) form a pair, so, fail to backtrack
+eq_node(_,N2):-			% symmetric on N2
+	p(_,N2),!,
+	fail.
+eq_node(N1,N2):-			% if you reach here, the is no inconsistency
+	asserta(p(N1,N2)). 			% hence pair N1, N2 is a legitimate one.
+eq_node(N1,N2):-			% if at a later moment, although pair N1, N2 is a legitimate one,
+    retract(p(N1,N2)),!, 		% the reasoning cannot conclude,
+    fail.						% remove it from the akb, and
+								% fail to backtrack and
+								% resume execution WITHOUT the pair in the akb
+
+

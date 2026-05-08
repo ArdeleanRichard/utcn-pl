c5300f841847a25dd2980823e2b85ce2b61adebd
eardelean <E.Ardelean@stratec.com>
2026-05-08 08:04:19 +0300

    c13

--- course/c13_code.pl
+++ course/c13_code.pl
@@ -40,20 +40,20 @@ try(0,X,Y,PCost,Cost,PPath,[X|PPath]):-
 %--------------------------------------------------
 % Hamiltonian cycle with B&B %
 %--------------------------------------------------
-
-
 % searches for N nodes to end in X the way; put in Cand a path containing just one element: [n(X,0,0)] 
-% start node X, estimated weight of hamilton EXi=0, number of nodes 0, NO parent. Expanded list empty. 
-hamilton(N,X,Way):-
-	search(N,X,[[n(X,0,0)]],[],Way).
+% start node X, estimated weight of hamilton EXi=0, number of nodes 0, NO parent. Expanded list empty.
+
+% ?- hamilton(6, a, Path)
+hamilton(N,X,Cycle):-
+	search(N,X,[[n(X,0,0)]],[],Cycle).
 
 
 % ?- search(6,a,[[n(a,0,0)]],[],Way).
-search(N,_,_,[Way|_],Way):- 			% stop with path the way in front of Expanded
-	Way=[n(_,N,_)|_],!. 				% if its length equals the number of nodes in graph
-search(N,X,[C|Cands],Exp,Way):-
-	expand(N,X,C,Cands,NewCands), 		% if not, expand best node so far = the element in front of current Cands
-	search(N,X,NewCands,[C|Exp],Way). 	% and continue after expansion with the new Cands
+search(N,_,_,[Cycle|_],Cycle):- 			% stop with path the way in front of Expanded
+	Cycle=[n(_,N,_)|_],!. 					% if its length equals the number of nodes in graph
+search(N,X,[C|Cands],Exp,Cycle):-
+	expand(N,X,C,Cands,NewCands), 			% if not, expand best node so far = the element in front of current Cands
+	search(N,X,NewCands,[C|Exp],Cycle). 	% and continue after expansion with the new Cands
 % C is a candidate path (best) which is expanded from last node added (first in list)
 % expand takes the best candidate path C and tries to find the next nodes
 % from these next nodes, all are collected and they update Cand into NewCand
@@ -62,21 +62,21 @@ search(N,X,[C|Cands],Exp,Way):-
 % 3rd arg is the best candidate list which is expanded, take first node (last added) and find neighbours
 expand(N,X,[n(Y,Len,Fi)|Cy],_,_):-
 	is_edge(Y,Z,W), 								% nondeterministically take Z,  first neighbor of Y (eventually all of them) and weight W
-	(not(member_thread(Z,Cy));(Len is N-1,Z=X)), 	% should NOT be already processed just if it is the source
+	(not(member_path(Z,Cy));(Len is N-1,Z=X)), 		% should NOT be already processed / just if it is the source
 	FiW is Fi+W, Len1 is Len+1, 					% estimate the new parameters
-	assertz(desc(n(Z,Len1,FiW))),					% put it in the akb
+	assertz(node(n(Z,Len1,FiW))),					% put it in the akb
 	fail.											% backtrack to a new neighbor of Y 
 expand(_,_,C,Cands,NewCands):-
-	collect(C,Cands,NewCands).						% start collecting from akb and place in NewCand
+	collect(C,Cands,NewCands).						% start collecting from akb and place in NewCands
 
 
-member_thread(X,[n(X,_,_)|_]). 						% checks if a node is in a list
-member_thread(X,[_|T]):- member_thread(X,T).
+member_path(X,[n(X,_,_)|_]). 						% checks if a node is in a list
+member_path(X,[_|T]):- member_path(X,T).
 
 
 % take the current best candidate list L, create a number of Lists that contain the next options and add them to candidate paths
 collect(C,Cands,NewCands):- 				% L is the parent, a whole path. Is Y in Candidate from clause 2 in search
-	retract(desc(Z)),!,	 					% take top from akb 
+	retract(node(Z)),!,	 					% take top from akb 
 	ins_ord_list([Z|C],Cands,IntCands),		% add in Cand with the whole path (+L, parent of path)
 	collect(C,IntCands,NewCands).			% continue with the Intermediate Candidate 
 collect(_,Cands,Cands).
